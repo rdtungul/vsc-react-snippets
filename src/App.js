4d908fdc3299a42ea20870b69cdc2356b9ce87fe
rdtungul <rosephdarl@gmail.com>
2023-10-12 20:42:50 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { useCallback } from 'react';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="is-blue">
+        <HelloWorld message="howdy world!" />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+function Alert({message, color}) {
+  const handleChange = useCallback(e => {
+    
+  },[])
+  return <div className={`is-alert ${color}`}>{message}</div>;
+}
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+function HelloWorld({message}) {
+
+  const [count, setCount] = useStatete(0)
+
+  useEffect(() => {
+    
+  }, []);
+
+  return <div>{message}</div>
+}
+
+HelloWorld.propTypes = {
+  message: PropTypes.string
+}
+
+export default HelloWorld;
