b4b23bc7a927b0b947e3216a8c30b85cfa7abe06
rdtungul <rosephdarl@gmail.com>
2023-10-16 22:14:16 +0800

    removing tailwind code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import 'tailwindcss/tailwind.css'
 function App() {
   return (
     <div>
-      <div className="is-blue">
-        <h1 className="min-h-screen flex items-center justify-center bg-green-400 opacity—75 text—lg xl:text-2xl">Hello World!</h1>
-      </div>
+      hi everyone
     </div>
   );
 }
